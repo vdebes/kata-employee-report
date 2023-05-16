adaa105e014287bda8c45d64f580d13db93f73ed
vdebes <vdebes@gmail.com>
2023-05-16 19:35:58 +0200

    refacto sorting

--- src/EmployeeProvider.php
+++ src/EmployeeProvider.php
@@ -30,13 +30,19 @@ class EmployeeProvider
     public function getAllEmployeesSorted(): array
     {
         $employees = self::getAllEmployees();
+        $employees = self::sortByNameAscending($employees);
+
+        return $employees;
+    }
+
+    private static function sortByNameAscending(array $employees): array
+    {
         usort(
             $employees,
             function (array $a, array $b): int {
                 return $a[0] <=> $b[0];
             }
         );
-
         return $employees;
     }
 }
