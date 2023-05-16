20decd54fb64dd609d2d94a34c2aef668ec711c1
vdebes <vdebes@gmail.com>
2023-05-16 19:33:28 +0200

    add get all employees sorted

--- src/EmployeeProvider.php
+++ src/EmployeeProvider.php
@@ -19,11 +19,24 @@ class EmployeeProvider
 
     public function getEmployeesOver18(): array
     {
-        return array_filter(
+        return array_values(array_filter(
             self::getAllEmployees(),
             function (array $employee): bool {
                 return $employee[1] >= 18;
             }
+        ));
+    }
+
+    public function getAllEmployeesSorted(): array
+    {
+        $employees = self::getAllEmployees();
+        usort(
+            $employees,
+            function (array $a, array $b): int {
+                return $a[0] <=> $b[0];
+            }
         );
+
+        return $employees;
     }
 }
--- tests/EmployeeProviderTest.php
+++ tests/EmployeeProviderTest.php
@@ -7,11 +7,23 @@ use PHPUnit\Framework\TestCase;
 
 class EmployeeProviderTest extends TestCase
 {
-    public function testGetEmployeesOver18()
+    public function testGetEmployeesOver18(): void
     {
         $employeeProvider = new EmployeeProvider();
         $employees = $employeeProvider->getEmployeesOver18();
 
         $this->assertCount(2, $employees);
+    }
+
+    public function testGetAllEmployeesSorted(): void
+    {
+        $employeeProvider = new EmployeeProvider();
+        $employees = $employeeProvider->getAllEmployeesSorted();
+
+        $this->assertCount(4, $employees);
+        $this->assertEquals('Max', $employees[0][0]);
+        $this->assertEquals('Mike', $employees[1][0]);
+        $this->assertEquals('Nina', $employees[2][0]);
+        $this->assertEquals('Sepp', $employees[3][0]);
     }
 }
